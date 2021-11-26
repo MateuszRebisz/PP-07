023e8b039ca2ddd9a499e5e2d9213142fe7436ac
MateuszRebisz <93045111+MateuszRebisz@users.noreply.github.com>
2021-11-26 20:35:27 +0100

    Add files via upload

---
+++ asd.py
@@ -0,0 +1,19 @@
+x = "Mateusz"
+y = "Rebisz"
+z = "UEK"
+a = "Informatyka Stosowana"
+
+file = open("info.txt", "w")
+file.write(x)
+file.write("\n")
+file.write(y)
+file.write("\n")
+file.write(z)
+file.write("\n")
+file.write(a)
+file.write("\n")
+
+
+
+    
+file.close()
